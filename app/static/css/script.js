0f8001b200cbb063f829c74cc74a4f1313a00b01
Jay <jaynair.alt@gmail.com>
2023-08-12 13:50:16 +1200

    added splash screen

---
+++ app/static/css/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        const splash = document.querySelector(".splash-screen");
+        splash.style.display = 'none';
+        document.body.style.overflow = 'auto'; // Enable scrolling again
+    }, 3000); // 3 seconds (the total time of both animations)
+});
