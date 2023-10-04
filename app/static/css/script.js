f2ff916bb81162fd9d6990b5170454b32e6c4962
Jay <jaynair.alt@gmail.com>
2023-10-04 22:09:20 +1300

    updated image review

--- app/static/css/script.js
+++ app/static/css/script.js
@@ -1,9 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM Content Loaded");
+    const reviewCards = document.querySelectorAll('.review-card');  // This line is added
+
     setTimeout(function() {
         const splash = document.querySelector(".splash-screen");
         splash.style.display = 'none';
         document.body.style.overflow = 'auto'; // Enable scrolling again
     }, 3000); // 3 seconds (the total time of both animations)
+
+    reviewCards.forEach(card => {
+        card.addEventListener('click', function() {
+            const isActive = card.classList.contains('active');
+            console.log("Card clicked");
+
+            reviewCards.forEach(innerCard => innerCard.classList.remove('active'));
+            document.querySelector('.content-wrapper').classList.remove('blurred');
+
+            if (!isActive) {
+                card.classList.add('active');
+                document.querySelector('.content-wrapper').classList.add('blurred');
+            }
+        });
+    });
+
+    document.body.addEventListener('click', function(e) {
+        if (!e.target.closest('.review-card')) {
+            reviewCards.forEach(card => card.classList.remove('active'));
+            document.querySelector('.content-wrapper').classList.remove('blurred');
+        }
+    }, true);
 });
-
-
